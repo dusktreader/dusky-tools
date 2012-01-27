c3d14463376d807bcfbcac75a3cedd08ee5a6e92
tucker.beck@gmail.com <tucker.beck@gmail.com@810eef66-2ca2-5086-595b-7ef49b4bcc66>
2012-01-27 15:31:50 +0000

    removed conflicting tools source

--- utilities.hpp
+++ utilities.hpp
@@ -145,3 +145,82 @@ T pow2( const T& val )
 {
     return val * val;
 }
+
+
+char* wrap_text( char* str, int right_margin, int left_padding )
+{
+    int span = right_margin - left_padding;
+    int i = 0;
+    int k = 0;
+    int j = 0;
+    int line_len = 0;
+    int len = strlen( str );
+    if( len == 0 )
+        return NULL;
+    char* wrap_str = (char*)malloc( len * sizeof(char) );
+    int   wrap_len = len;
+    bool hyphenate = 0;
+
+    do
+    {
+        if( str[i] == ' ' )
+        {
+            i++;
+        }
+        else
+        {
+            /// @note:  The +2 is for the newline character and the null-terminating character;
+            if( k + right_margin + 2 > wrap_len )
+            {
+                wrap_len += right_margin + 2;
+                wrap_str = (char*)realloc( wrap_str, wrap_len * sizeof( char ) );
+            }
+
+
+            if( i + span >= len )
+            {
+                hyphenate = 0;
+                line_len = len - i;
+            }
+            else
+            {
+                j = span;
+                do
+                {
+                    if( str[ i + j ] == ' ' )
+                    {
+                        hyphenate = 0;
+                        line_len = j;
+                        break;
+                    }
+                    else if( j == 0 )
+                    {
+                        hyphenate = 1;
+                        line_len = span - 1;
+                        break;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                } while( 1 );
+            }
+
+            memset( wrap_str + k, ' ', left_padding * sizeof( char ) );
+            k += left_padding;
+
+            memcpy( wrap_str + k, str + i, line_len * sizeof( char ) );
+            k += line_len;
+            i += line_len;
+
+            if( hyphenate )
+                wrap_str[k++] = '-';
+
+            wrap_str[k++] = '\n';
+
+            wrap_str[k] = '\0';
+        }
+    } while( i < len );
+
+    return wrap_str;
+}
