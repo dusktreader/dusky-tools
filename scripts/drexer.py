252e800bcec5fd8d9588518a3ccac684b9898427
tucker.beck@gmail.com <tucker.beck@gmail.com@810eef66-2ca2-5086-595b-7ef49b4bcc66>
2012-03-22 03:53:00 +0000

    typo

--- scripts/drexer.py
+++ scripts/drexer.py
@@ -14,7 +14,7 @@ def main():
         config = ConfigParser() 
         config.read( os.path.dirname( __file__ ) + os.sep + 'drexer.cfg' )
         default_archive_dir = config.get( 'defaults', 'archive_dir' )
-        default_sites_dir   = config.get( 'defaults', 'extension_dir' )
+        default_extension_dir   = config.get( 'defaults', 'extension_dir' )
     except:
         pass
 
